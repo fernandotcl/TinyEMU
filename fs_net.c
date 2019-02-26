28044fa2faa3ca0e1cea357db6afcfb223b0bb6b
Fernando Lemos <fernandotcl@gmail.com>
2019-02-26 20:26:43 +0100

    Fix warnings building with clang

--- fs_net.c
+++ fs_net.c
@@ -1783,11 +1783,11 @@ static void dump_loaded_file(FSDevice *fs1, FSINode *n)
     if (n->u.reg.size >= ARCHIVE_SIZE_MAX) {
         /* exclude large files from archive */
         /* add indicative size */
-        fprintf(fs->dump_preload_file, "  %s %" PRId64 "\n",
+        fprintf(fs->dump_preload_file, "  %s %zd\n",
                 fname, n->u.reg.size);
         fflush(fs->dump_preload_file);
     } else {
-        fprintf(fs->dump_preload_archive_file, "  %s %" PRId64 " %" PRIx64 "\n",
+        fprintf(fs->dump_preload_archive_file, "  %s %zd %llx\n",
                 n->u.reg.filename, n->u.reg.size, n->u.reg.file_id);
         fflush(fs->dump_preload_archive_file);
         fwrite(n->u.reg.fbuf.data, 1, n->u.reg.size, fs->dump_archive_file);
