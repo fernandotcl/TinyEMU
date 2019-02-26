c954213439ab4e9ea91d2eef32153e0201866c28
Fernando Lemos <fernandotcl@gmail.com>
2019-02-26 21:41:03 +0100

    Translate SDL scancodes to evdev keycodes
    Fixes keyboard input on the Mac.

--- sdl.c
+++ sdl.c
@@ -78,31 +78,185 @@ static void sdl_update(FBDevice *fb_dev, void *opaque,
     SDL_RenderPresent(renderer);
 }
 
-#if defined(_WIN32)
-
 static int sdl_get_keycode(const SDL_KeyboardEvent *ev)
 {
-    return ev->keysym.scancode;
-}
-
-#else
-
-/* we assume Xorg is used with a PC keyboard. Return 0 if no keycode found. */
-static int sdl_get_keycode(const SDL_KeyboardEvent *ev)
-{
-    int keycode;
-    keycode = ev->keysym.scancode;
-    if (keycode < 9) {
-        keycode = 0;
-    } else if (keycode < 127 + 8) {
-        keycode -= 8;
-    } else {
-        keycode = 0;
+    switch (ev->keysym.scancode) {
+        case SDL_SCANCODE_ESCAPE: return 1;
+        case SDL_SCANCODE_1: return 2;
+        case SDL_SCANCODE_2: return 3;
+        case SDL_SCANCODE_3: return 4;
+        case SDL_SCANCODE_4: return 5;
+        case SDL_SCANCODE_5: return 6;
+        case SDL_SCANCODE_6: return 7;
+        case SDL_SCANCODE_7: return 8;
+        case SDL_SCANCODE_8: return 9;
+        case SDL_SCANCODE_9: return 10;
+        case SDL_SCANCODE_0: return 11;
+        case SDL_SCANCODE_MINUS: return 12;
+        case SDL_SCANCODE_EQUALS: return 13;
+        case SDL_SCANCODE_BACKSPACE: return 14;
+        case SDL_SCANCODE_TAB: return 15;
+        case SDL_SCANCODE_Q: return 16;
+        case SDL_SCANCODE_W: return 17;
+        case SDL_SCANCODE_E: return 18;
+        case SDL_SCANCODE_R: return 19;
+        case SDL_SCANCODE_T: return 20;
+        case SDL_SCANCODE_Y: return 21;
+        case SDL_SCANCODE_U: return 22;
+        case SDL_SCANCODE_I: return 23;
+        case SDL_SCANCODE_O: return 24;
+        case SDL_SCANCODE_P: return 25;
+        case SDL_SCANCODE_LEFTBRACKET: return 26;
+        case SDL_SCANCODE_RIGHTBRACKET: return 27;
+        case SDL_SCANCODE_RETURN: return 28;
+        case SDL_SCANCODE_LCTRL: return 29;
+        case SDL_SCANCODE_A: return 30;
+        case SDL_SCANCODE_S: return 31;
+        case SDL_SCANCODE_D: return 32;
+        case SDL_SCANCODE_F: return 33;
+        case SDL_SCANCODE_G: return 34;
+        case SDL_SCANCODE_H: return 35;
+        case SDL_SCANCODE_J: return 36;
+        case SDL_SCANCODE_K: return 37;
+        case SDL_SCANCODE_L: return 38;
+        case SDL_SCANCODE_SEMICOLON: return 39;
+        case SDL_SCANCODE_APOSTROPHE: return 40;
+        case SDL_SCANCODE_GRAVE: return 41;
+        case SDL_SCANCODE_LSHIFT: return 42;
+        case SDL_SCANCODE_BACKSLASH: return 43;
+        case SDL_SCANCODE_Z: return 44;
+        case SDL_SCANCODE_X: return 45;
+        case SDL_SCANCODE_C: return 46;
+        case SDL_SCANCODE_V: return 47;
+        case SDL_SCANCODE_B: return 48;
+        case SDL_SCANCODE_N: return 49;
+        case SDL_SCANCODE_M: return 50;
+        case SDL_SCANCODE_COMMA: return 51;
+        case SDL_SCANCODE_PERIOD: return 52;
+        case SDL_SCANCODE_SLASH: return 53;
+        case SDL_SCANCODE_RSHIFT: return 54;
+        case SDL_SCANCODE_KP_MULTIPLY: return 55;
+        case SDL_SCANCODE_LALT: return 56;
+        case SDL_SCANCODE_SPACE: return 57;
+        case SDL_SCANCODE_CAPSLOCK: return 58;
+        case SDL_SCANCODE_F1: return 59;
+        case SDL_SCANCODE_F2: return 60;
+        case SDL_SCANCODE_F3: return 61;
+        case SDL_SCANCODE_F4: return 62;
+        case SDL_SCANCODE_F5: return 63;
+        case SDL_SCANCODE_F6: return 64;
+        case SDL_SCANCODE_F7: return 65;
+        case SDL_SCANCODE_F8: return 66;
+        case SDL_SCANCODE_F9: return 67;
+        case SDL_SCANCODE_F10: return 68;
+        case SDL_SCANCODE_NUMLOCKCLEAR: return 69;
+        case SDL_SCANCODE_SCROLLLOCK: return 70;
+        case SDL_SCANCODE_KP_7: return 71;
+        case SDL_SCANCODE_KP_8: return 72;
+        case SDL_SCANCODE_KP_9: return 73;
+        case SDL_SCANCODE_KP_MINUS: return 74;
+        case SDL_SCANCODE_KP_4: return 75;
+        case SDL_SCANCODE_KP_5: return 76;
+        case SDL_SCANCODE_KP_6: return 77;
+        case SDL_SCANCODE_KP_PLUS: return 78;
+        case SDL_SCANCODE_KP_1: return 79;
+        case SDL_SCANCODE_KP_2: return 80;
+        case SDL_SCANCODE_KP_3: return 81;
+        case SDL_SCANCODE_KP_0: return 82;
+        case SDL_SCANCODE_KP_PERIOD: return 83;
+        case SDL_SCANCODE_LANG5: return 85;
+        case SDL_SCANCODE_NONUSBACKSLASH: return 86;
+        case SDL_SCANCODE_F11: return 87;
+        case SDL_SCANCODE_F12: return 88;
+        case SDL_SCANCODE_INTERNATIONAL1: return 89;
+        case SDL_SCANCODE_LANG3: return 90;
+        case SDL_SCANCODE_LANG4: return 91;
+        case SDL_SCANCODE_INTERNATIONAL4: return 92;
+        case SDL_SCANCODE_INTERNATIONAL2: return 93;
+        case SDL_SCANCODE_INTERNATIONAL5: return 94;
+        case SDL_SCANCODE_KP_ENTER: return 96;
+        case SDL_SCANCODE_RCTRL: return 97;
+        case SDL_SCANCODE_KP_DIVIDE: return 98;
+        case SDL_SCANCODE_SYSREQ: return 99;
+        case SDL_SCANCODE_RALT: return 100;
+        case SDL_SCANCODE_HOME: return 102;
+        case SDL_SCANCODE_UP: return 103;
+        case SDL_SCANCODE_PAGEUP: return 104;
+        case SDL_SCANCODE_LEFT: return 105;
+        case SDL_SCANCODE_RIGHT: return 106;
+        case SDL_SCANCODE_END: return 107;
+        case SDL_SCANCODE_DOWN: return 108;
+        case SDL_SCANCODE_PAGEDOWN: return 109;
+        case SDL_SCANCODE_INSERT: return 110;
+        case SDL_SCANCODE_DELETE: return 111;
+        case SDL_SCANCODE_MUTE: return 113;
+        case SDL_SCANCODE_VOLUMEDOWN: return 114;
+        case SDL_SCANCODE_VOLUMEUP: return 115;
+        case SDL_SCANCODE_POWER: return 116;
+        case SDL_SCANCODE_KP_EQUALS: return 117;
+        case SDL_SCANCODE_KP_PLUSMINUS: return 118;
+        case SDL_SCANCODE_PAUSE: return 119;
+        case SDL_SCANCODE_KP_COMMA: return 121;
+        case SDL_SCANCODE_LANG1: return 122;
+        case SDL_SCANCODE_LANG2: return 123;
+        case SDL_SCANCODE_INTERNATIONAL3: return 124;
+        case SDL_SCANCODE_LGUI: return 125;
+        case SDL_SCANCODE_RGUI: return 126;
+        case SDL_SCANCODE_APPLICATION: return 127;
+        case SDL_SCANCODE_STOP: return 128;
+        case SDL_SCANCODE_AGAIN: return 129;
+        case SDL_SCANCODE_UNDO: return 131;
+        case SDL_SCANCODE_COPY: return 133;
+        case SDL_SCANCODE_PASTE: return 135;
+        case SDL_SCANCODE_FIND: return 136;
+        case SDL_SCANCODE_CUT: return 137;
+        case SDL_SCANCODE_HELP: return 138;
+        case SDL_SCANCODE_MENU: return 139;
+        case SDL_SCANCODE_CALCULATOR: return 140;
+        case SDL_SCANCODE_SLEEP: return 142;
+        case SDL_SCANCODE_APP1: return 148;
+        case SDL_SCANCODE_APP2: return 149;
+        case SDL_SCANCODE_WWW: return 150;
+        case SDL_SCANCODE_MAIL: return 155;
+        case SDL_SCANCODE_AC_BOOKMARKS: return 156;
+        case SDL_SCANCODE_COMPUTER: return 157;
+        case SDL_SCANCODE_AC_BACK: return 158;
+        case SDL_SCANCODE_AC_FORWARD: return 159;
+        case SDL_SCANCODE_EJECT: return 161;
+        case SDL_SCANCODE_AUDIONEXT: return 163;
+        case SDL_SCANCODE_AUDIOPLAY: return 164;
+        case SDL_SCANCODE_AUDIOPREV: return 165;
+        case SDL_SCANCODE_AUDIOSTOP: return 166;
+        case SDL_SCANCODE_AUDIOREWIND: return 168;
+        case SDL_SCANCODE_AC_HOME: return 172;
+        case SDL_SCANCODE_AC_REFRESH: return 173;
+        case SDL_SCANCODE_KP_LEFTPAREN: return 179;
+        case SDL_SCANCODE_KP_RIGHTPAREN: return 180;
+        case SDL_SCANCODE_F13: return 183;
+        case SDL_SCANCODE_F14: return 184;
+        case SDL_SCANCODE_F15: return 185;
+        case SDL_SCANCODE_F16: return 186;
+        case SDL_SCANCODE_F17: return 187;
+        case SDL_SCANCODE_F18: return 188;
+        case SDL_SCANCODE_F19: return 189;
+        case SDL_SCANCODE_F20: return 190;
+        case SDL_SCANCODE_F21: return 191;
+        case SDL_SCANCODE_F22: return 192;
+        case SDL_SCANCODE_F23: return 193;
+        case SDL_SCANCODE_F24: return 194;
+        case SDL_SCANCODE_AUDIOFASTFORWARD: return 208;
+        case SDL_SCANCODE_AC_SEARCH: return 217;
+        case SDL_SCANCODE_ALTERASE: return 222;
+        case SDL_SCANCODE_CANCEL: return 223;
+        case SDL_SCANCODE_BRIGHTNESSDOWN: return 224;
+        case SDL_SCANCODE_BRIGHTNESSUP: return 225;
+        case SDL_SCANCODE_DISPLAYSWITCH: return 227;
+        case SDL_SCANCODE_KBDILLUMTOGGLE: return 228;
+        case SDL_SCANCODE_KBDILLUMDOWN: return 229;
+        case SDL_SCANCODE_KBDILLUMUP: return 230;
+        default: return 0;
     }
-    return keycode;
 }
-
-#endif
 
 /* release all pressed keys */
 static void sdl_reset_keys(VirtMachine *m)
