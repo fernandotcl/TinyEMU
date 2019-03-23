cd86ae55341791b84c9f80f40885f79c6b88a5e6
Jim Huang <jserv.tw@gmail.com>
2019-03-23 19:51:51 +0800

    Optimize ctz32

--- cutils.h
+++ cutils.h
@@ -170,17 +170,39 @@ static inline uint32_t cpu_to_be32(uint32_t v)
 }
 #endif
 
-/* XXX: optimize */
-static inline int ctz32(uint32_t a)
+static inline int ctz32(uint32_t val)
 {
-    int i;
-    if (a == 0)
-        return 32;
-    for(i = 0; i < 32; i++) {
-        if ((a >> i) & 1)
-            return i;
+#if defined(__GNUC__) && __GNUC__ >= 4
+    return val ? __builtin_ctz(val) : 32;
+#else
+    /* Binary search for the trailing one bit.  */
+    int cnt;
+    cnt = 0;
+    if (!(val & 0x0000FFFFUL)) {
+        cnt += 16;
+        val >>= 16;
     }
-    return 32;
+    if (!(val & 0x000000FFUL)) {
+        cnt += 8;
+        val >>= 8;
+    }
+    if (!(val & 0x0000000FUL)) {
+        cnt += 4;
+        val >>= 4;
+    }
+    if (!(val & 0x00000003UL)) {
+        cnt += 2;
+        val >>= 2;
+    }
+    if (!(val & 0x00000001UL)) {
+        cnt++;
+        val >>= 1;
+    }
+    if (!(val & 0x00000001UL)) {
+        cnt++;
+    }
+    return cnt;
+#endif
 }
 
 
